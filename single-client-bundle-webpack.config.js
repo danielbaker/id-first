24d0260219f548592270c5dd3340faedc2d18da7
Dan Baker <dan.baker@auth0.com>
2018-11-20 11:34:23 +1100

    fix bundle

--- single-client-bundle-webpack.config.js
+++ single-client-bundle-webpack.config.js
@@ -3,9 +3,10 @@ const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 const glob = require("glob");
 
 module.exports = {
+  mode: "production",
   entry: {
     "bundle.js": glob
-      .sync("build/static/?(js|css)/main.*.?(js|css)")
+      .sync("build/static/?(js|css)/*chunk.?(js|css)")
       .map(f => path.resolve(__dirname, f))
   },
   output: {
